0c2a9305cc92409848182be19c15c1f3a001c924
Geoo Chi <geoochi@geoochi.com>
2025-06-22 11:03:23 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,25 @@
 import MapComponent from './MapComponent'
+import './globals.css'
 
 export default function Page() {
   const mapboxToken = process.env.MAPBOX_ACCESS_TOKEN
 
-  if (!mapboxToken) {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          fontSize: '18px',
-          color: '#666',
-        }}
-      >
-        Mapbox access token not configured
-      </div>
-    )
-  }
-
   return (
-    <div>
-      <a id='logo' href='https://github.com/geoochi/track_fly' target='_blank'>
-        <img src='/github.svg' />
-      </a>
-      <MapComponent mapboxToken={mapboxToken} />
-    </div>
+    <>
+      {mapboxToken ? (
+        <div>
+          <a
+            id='logo'
+            href='https://github.com/geoochi/track_fly'
+            target='_blank'
+          >
+            <img src='/github.svg' />
+          </a>
+          <MapComponent mapboxToken={mapboxToken} />
+        </div>
+      ) : (
+        <div id='no-token'>Mapbox access token not configured</div>
+      )}
+    </>
   )
 }
